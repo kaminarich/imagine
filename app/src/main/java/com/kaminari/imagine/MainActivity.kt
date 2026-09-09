27081b34aacda56068cfb53b0bc274c0dcd5fd49
kaminarich <kaminarich@users.noreply.github.com>
2026-09-09 18:06:09 +0700

    Fix: add missing alpha and drawWithContent imports

--- app/src/main/java/com/kaminari/imagine/MainActivity.kt
+++ app/src/main/java/com/kaminari/imagine/MainActivity.kt
@@ -24,7 +24,9 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
