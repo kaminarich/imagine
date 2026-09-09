3d41df5691bdb4dbd02fb9fff5ffda9f7e0ff8c2
kaminarich <kaminarich@users.noreply.github.com>
2026-09-09 20:56:24 +0700

    Fix: restore RoundedCornerShape import

--- app/src/main/java/com/kaminari/imagine/MainActivity.kt
+++ app/src/main/java/com/kaminari/imagine/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.gestures.drag
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
